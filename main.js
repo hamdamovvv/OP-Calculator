6f81745767ab17f104b7a2b1774b5bfc3ebd1c94
hamdamovvv <akhmadkhamdamov000@gmail.com>
2023-05-04 17:09:00 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+//переменная в которой хранится наши математические действия
+let op
+// Фнукция для результата
+function calc() {
+    //Переменная для результата
+    let result
+    //Получаем первое и второе число
+    let num1 = Number(document.getElementById('num1').value)
+    let num2 = Number(document.getElementById('num2').value)
+    //смотрим, что было в переменной
+    //с действием, и действуем исходя из этого
+    switch (op) {
+        case '+' :
+        result = num1 + num2
+        break;
+        case '-' :
+        result = num1 - num2
+        break; case '*' :
+        result = num1 * num2
+        break;
+        case '/' :
+            if (num2 === 0) {
+                document.getElementById('result').textContent = "Ошибка"
+                return
+            }
+        result = num1 / num2
+        break
+        default :
+            document.getElementById('result')
+                .textContent = 'Ошибка, выберите действие'
+    }
+    // отправляем результат на страницу
+    document.getElementById('result').innerText = result
+}
